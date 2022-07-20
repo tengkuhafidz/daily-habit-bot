4797d93207a1a72a63b415aaced42ac5f1cf6224
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2022-07-20 16:09:53 +0800

    change config lib

--- configs/appConfig.ts
+++ configs/appConfig.ts
@@ -1,6 +1,6 @@
-import { config } from 'https://deno.land/std/dotenv';
+import { config } from 'https://deno.land/std@0.148.0/dotenv/mod.ts';
 
-config({ export: true })
+await config({ export: true })
 
 export const appConfig = {
     firebaseConfig: JSON.parse(Deno.env.get("FIREBASE_CONFIG") as string),
